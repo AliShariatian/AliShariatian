303df768eb6b7f310a4c2952754c3ba2b5bf44d3
Ali Shariatian <shariatian92@gmail.com>
2024-01-17 17:12:20 +0330

    feat: add hero content component

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,9 +1,12 @@
+import HeroContent from "../heroContent/HeroContent";
+
 function Hero() {
    return (
       <div className="relative flex flex-col h-full w-full">
          <video autoPlay muted loop className="absolute top-[-340px] left-0 w-full h-full object-cover rotate-180 z-[1]">
             <source src="/video/blackhole.webm" type="video/webm" />
          </video>
+         <HeroContent />
       </div>
    );
 }
