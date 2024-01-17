83576422d331b2b6d03fca14dbb6679f36ef701a
Ali Shariatian <shariatian92@gmail.com>
2024-01-17 15:58:23 +0330

    style: add inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const fontInter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
    title: "Ali Shariatian | Front-end Developer",
@@ -9,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
    return (
       <html lang="en">
-         <body>{children}</body>
+         <body className={`${fontInter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}>{children}</body>
       </html>
    );
 }
