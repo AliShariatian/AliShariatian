72204eeda38366259ae67dd15e4f80d5d65a6270
Ali Shariatian <shariatian92@gmail.com>
2024-01-18 02:06:24 +0330

    style: change tools font size

--- components/sub/Tools.tsx
+++ components/sub/Tools.tsx
@@ -2,7 +2,7 @@ function Tools({ tools }: { tools: string[] }) {
    return (
       <>
          {tools.map((tool) => (
-            <div className="py-1 px-4 text-white bg-slate-800 rounded-full">{tool}</div>
+            <div className="py-1 px-4 text-white text-sm bg-slate-800 rounded-full">{tool}</div>
          ))}
       </>
    );
