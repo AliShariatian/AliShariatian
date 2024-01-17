8d00c5483e18ea2151c27dc62a641456621445ad
Ali Shariatian <shariatian92@gmail.com>
2024-01-17 17:58:11 +0330

    feat: add stars background to view

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Hero from "@/components/main/Hero";
 
 function HomePage() {
    return (
-      <main className="h-full w-full">
-         <div className="flex flex-col gap-20 h-[850px] ">
+      <main className="size-full">
+         <div className="flex flex-col gap-20 h-[850px]">
             <Hero />
          </div>
       </main>
