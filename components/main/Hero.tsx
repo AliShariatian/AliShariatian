02093c912f93d7262ba3397a7e132ff849e17fab
Ali Shariatian <shariatian92@gmail.com>
2024-01-18 02:20:27 +0330

    style: fix hero top position

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -3,7 +3,7 @@ import HeroContent from "../sub/HeroContent";
 function Hero() {
    return (
       <div className="relative flex flex-col h-full w-full">
-         <video autoPlay muted loop className="absolute -top-[287px] md:top-[-352px] left-0 w-full h-full object-cover rotate-180 z-[1]">
+         <video autoPlay muted loop className="absolute -top-[296px] md:top-[-360px] left-0 w-full h-full object-cover rotate-180 z-[1]">
             <source src="/video/blackhole.webm" type="video/webm" />
          </video>
          <HeroContent />
