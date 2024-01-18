1da9d91e67dd0eedf9687b8c3b1610729383c732
Ali Shariatian <shariatian92@gmail.com>
2024-01-18 15:46:24 +0330

    fix: add view port for navbar and galaxy in top bar

--- components/main/NavBar.tsx
+++ components/main/NavBar.tsx
@@ -3,13 +3,13 @@ import { TopNavBarSocials } from "@/constants";
 
 function NavBar() {
    return (
-      <nav className="w-full h-16 fixed top-0 shadow-sm shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-2 md:px-32">
+      <nav className="w-full h-16 fixed top-0 shadow-sm shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-2 lg:px-32">
          <div className="size-full flex items-center justify-center md:justify-between m-auto px-3">
             <a href="#about-me" className="hidden md:block text-md md:text-xl font-extrabold ml-2 text-white">
                <h1>Ali Shariatian</h1>
             </a>
             <div className="flex items-center justify-between h-full">
-               <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] md:mr-4 px-5 py-[10px] rounded-full text-gray-300">
+               <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] lg:mr-4 px-5 py-[10px] rounded-full text-gray-300">
                   <a href="#about-me" className="cursor-pointer">
                      About Me
                   </a>
